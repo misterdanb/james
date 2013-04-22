b9c7e5295dff28ae973dbab3edae530cde512cc3
danb <unknown>
2013-04-22 20:13:57 +0200

    - ups, i forgot to add InstructionTable.hpp

---
+++ core/cpu/InstructionTable.hpp
@@ -0,0 +1,19 @@
+#ifndef _InstructionTable_hpp_
+#define _InstructionTable_hpp_
+
+namespace gbc
+{
+	namespace core
+	{
+		namespace cpu
+		{
+			enum class InstructionTable : int
+			{
+				DEFAULT = 0,
+				CB = 1
+			};
+		}
+	}
+}
+
+#endif
