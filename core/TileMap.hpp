680fec1fc86927fedbafe02acecdca6e47754ec3
danb <unknown>
2013-04-22 20:17:51 +0200

    - ups, forgot TileMap.hpp, too, ladida

---
+++ core/TileMap.hpp
@@ -0,0 +1,22 @@
+#ifndef _TileMap_hpp_
+#define _TileMap_hpp_
+
+#include "Tile.hpp"
+
+namespace gbc
+{
+	namespace core
+	{
+		class TileMap
+		{
+		public:
+			static const int WIDTH = 32;
+			static const int HEIGHT = 32;
+		
+		public:
+			int data[WIDTH][HEIGHT];
+		};
+	}
+}
+
+#endif
